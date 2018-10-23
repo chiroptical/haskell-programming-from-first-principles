6937d6da38650075cd8e1a386064240b3da27816
Barry Moore <moore0557@gmail.com>
2018-10-23 08:53:31 -0400

    Working on Chapter 14

--- Chapter14/quickcheck/src/Main.hs
+++ Chapter14/quickcheck/src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+-- import Test.Hspec
+import Test.QuickCheck
+
+-- main :: IO ()
+-- main = hspec $
+--   it "x + 1 is always greater than x" $
+--     property $ \x -> x + 1 > (x :: Integer)
+
+prop_additionGreater :: Int -> Bool
+prop_additionGreater x = x + 1 > x
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = quickCheck prop_additionGreater
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-On Section 14.4, Enter QuickCheck, Stack project made
+On Section 14.5, Morse code, page 562 "Make sure it's all working"
