ae0746301231c46e3beed2a09a533b9ef4e280fe
Barry Moore <moore0557@gmail.com>
2019-05-21 21:43:50 -0400

    Finish Morra Game

--- Chapter26/morra/src/Main.hs
+++ Chapter26/morra/src/Main.hs
@@ -1,55 +1,112 @@
 module Main where
 
-import Control.Monad.Trans.State
-import Control.Monad.IO.Class
-import System.Random (randomRIO)
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.State
+import           System.Random             (randomRIO)
 
 -- Rules:
 -- User picks evens or odds
 -- User picks 0 to 5
 -- Computer picks randomly from 0 to 5
 -- Sum if even = even wins
--- Sum if off = odd wins
+-- Sum if odd = odd wins
 -- First to 6 points wins
-
-
-data Player =
-    Human
-  | Computer
-  deriving Show
+data EvenOdd
+  = Even
+  | Odd
+  deriving (Show, Eq)
 
 data Game =
   Game
-    { human :: Integer
-    , computer :: Integer
-    } deriving Show
+    { humanScore    :: Integer
+    , computerScore :: Integer
+    , evenOdd       :: EvenOdd
+    , message       :: String
+    }
+
+instance Show Game where
+  show g =
+    concat
+      [ "Morra ("
+      , show (evenOdd g)
+      , " wins) Scores:\n"
+      , "Yours: "
+      , show (humanScore g)
+      , "\n"
+      , "Computers: "
+      , show (computerScore g)
+      , "\n"
+      ]
 
 -- This should be Either so we can gracefully deal with errors
-handleGuess :: String -> Maybe Integer
-handleGuess "0" = Just 0
-handleGuess "1" = Just 1
-handleGuess "2" = Just 2
-handleGuess "3" = Just 3
-handleGuess "4" = Just 4
-handleGuess "5" = Just 5
-handleGuess _ = error "You only have 5 fingers!"
+parseGuess :: String -> Maybe Integer
+parseGuess "0" = Just 0
+parseGuess "1" = Just 1
+parseGuess "2" = Just 2
+parseGuess "3" = Just 3
+parseGuess "4" = Just 4
+parseGuess "5" = Just 5
+parseGuess _   = Nothing
+
+handleEvenOdd :: String -> Maybe EvenOdd
+handleEvenOdd "even" = Just Even
+handleEvenOdd "odd"  = Just Odd
+handleEvenOdd _      = Nothing
+
+integerToEvenOdd :: Integer -> EvenOdd
+integerToEvenOdd x =
+  if (x `mod` 2 == 0)
+    then Even
+    else Odd
+
+scoreRound :: Integer -> Integer -> Game -> Game
+scoreRound humanGuess compGuess game
+  | integerToEvenOdd (humanGuess + compGuess) == gameChoice =
+    Game (hScore + 1) cScore gameChoice "Good guess!"
+  | otherwise = Game hScore (cScore + 1) gameChoice "Bad guess nerd!"
+  where
+    gameChoice = evenOdd game
+    hScore = humanScore game
+    cScore = computerScore game
+
+handleGuess :: Maybe Integer -> Integer -> Game -> Either String Game
+handleGuess (Just humanGuess) compGuess game =
+  Right $ scoreRound humanGuess compGuess game
+handleGuess Nothing _ _ = Left "You only have 5 fingers! Choose 0 to 5:"
+
+declareWinner :: Game -> Either String Game
+declareWinner Game {humanScore = 6}    = Left "You win!"
+declareWinner Game {computerScore = 6} = Left "You lose! #sad"
+declareWinner g                        = Right g
 
 playGame :: StateT Game IO ()
 playGame = do
   game <- get
   liftIO $ putStrLn "Guess a number between 0 and 5"
-  guess' <- liftIO $ getLine
-  let humanGuess = handleGuess guess'
-      computerGuess = randomRIO (0 :: Integer, 5)
-  undefined
-
+  humanGuess <- liftIO $ parseGuess <$> getLine
+  computerGuess <- liftIO $ randomRIO (0 :: Integer, 5)
+  case handleGuess humanGuess computerGuess game of
+    Right newGame -> do
+      liftIO $ putStrLn $ message newGame
+      put newGame
+    Left m -> do
+      liftIO $ putStrLn m
+      playGame
+  newGame <- get
+  case declareWinner newGame of
+    Left msg -> liftIO $ putStrLn msg
+    Right g -> do
+      liftIO $ print g
+      playGame
 
 main :: IO ()
 main = do
-  putStrLn "Choose evens or odds"
-  evensOdds <- getLine
-  if (evensOdds == "evens" || evensOdds == "odds")
-  then putStrLn $ "You are the " ++ evensOdds ++ " player, good luck!"
-  else error "Must choose evens or odds"
-  results <- execStateT playGame $ Game 0 0
-  print results
+  putStrLn "Choose 'even' or 'odd'"
+  evenOdd' <- handleEvenOdd <$> getLine
+  case evenOdd' of
+    Just eo -> do
+      results <- execStateT playGame $ Game 0 0 eo ""
+      print results
+    Nothing -> do
+      putStrLn "Must chose either 'even' or 'odd'"
+      main
